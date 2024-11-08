7c175e10360a7a4c2affb2508fba5b6b268e7f26
Daniel Fortich <fortichdaniel16@gmail.com>
2024-11-08 16:51:52 -0500

    Reverse String

--- src/main.rs
+++ src/main.rs
@@ -10,4 +10,12 @@ fn main() {
     println!("{}", exercism::hello::hello());
 
     print_separator();
+
+    let reverse_string_inputs = vec!["robot", "Ramen", "I'm hungry!", "racecar", "drawer", "子猫", "Würstchenstand", "ผู้เขียนโปรแกรม"];
+
+    for input in reverse_string_inputs {
+        println!("Reverse of ({}): {}", input, exercism::reverse_string::reverse(input));
+    }
+
+    print_separator();
 }
